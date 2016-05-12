31d2bd373cba43ff8b511790b52722156ca24dff
vrtornisiello <vrtornisiello@gmail.com>
2016-05-12 15:29:36 -0300

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,33 @@
+#include<SDL/SDL2.h>
+#include<stidio.h>
+#include<math.h>
+
+typedef struct coordenada{
+	int x, y;
+} Coordenada;
+
+typedef struct objetos{
+	Coordenada inicio;
+	Coordenada vetord;
+	int r;
+} Objetos;
+
+typedef struct nave{
+	Coordenada posicao;
+	float angulo;
+} Nave;
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+	
